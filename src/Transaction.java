ce86755c4309269e2c73ef78780764603fa9fc65
manish <manishengi25@gmail.com>
2015-07-27 19:18:09 +0530

    TranscationFileAdded

---
+++ src/Transaction.java
@@ -0,0 +1,12 @@
+package trackingnumberproblem;
+
+import trackingnumberproblem.Range;
+
+public class Transaction
+{
+    private Range range;
+    private char statusCode;
+    private char transferCode;
+    private boolean isDeleted;
+};
+       
