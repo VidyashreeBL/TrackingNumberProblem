81b5b274dfb24e431fff322460e4dac252dbef55
VidyashreeBL <vidu_friend@hotmail.com>
2015-07-28 09:19:27 +0530

    Changing access specifiers of instance variables to protected

--- src/Transaction.java
+++ src/Transaction.java
@@ -4,11 +4,11 @@ import trackingnumberproblem.Range;
 
 public class Transaction
 {
-    private Range range;
-    private char statusCode;
-    private char transferCode;
-    private boolean isDeleted;
-    private String transactionHeader;
+    protected Range range;
+    protected char statusCode;
+    protected char transferCode;
+    protected boolean isDeleted;
+    protected String transactionHeader;
     
     public Transaction(Range r, char s, char t){
         range = r;
@@ -16,27 +16,6 @@ public class Transaction
         transferCode = t;
         isDeleted = false;
     }
-    public String getTransactionHeader() {
-        return transactionHeader;
-    }
-    
-    public char getTransferCode() {
-        return transferCode;
-    }
-    
-     public char getStatusCode() {
-        return statusCode;
-    }
-     
-     public boolean getisDeleted() {
-        return isDeleted;
-    } 
-    
-     public Range getRange() {
-         return range;
-     }
-             
-    
             
 }
 
