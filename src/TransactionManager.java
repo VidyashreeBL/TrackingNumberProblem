b9442abe3f53f9707cdd0984f1f6f8420da5dce0
VidyashreeBL <vidu_friend@hotmail.com>
2015-07-28 08:36:21 +0530

    Created TransactionManager class

---
+++ src/TransactionManager.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class TransactionManager {
+    ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+    
+    public void addNewTransaction(Transaction t){
+        
+    }
+    
+    public void displayTransactions(){
+        
+    }
+}
